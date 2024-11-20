30ed181025f4b5693f32602728837cdb9e7934de
surajkr20 <suraj71308kumar@gmail.com>
2024-11-20 08:41:21 +0530

    add hamburger animation

--- portfolio/src/pages/Navbar/Navbar.jsx
+++ portfolio/src/pages/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 
+import { useRef } from "react";
 import Logo from "../../components/Logo/Logo";
 import "./Navbar.css";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
+
+  const menu = useRef()
+  const mobile = useRef()
+
   return (
     <>
       <nav className="nav-container flex flex-row items-center justify-between w-full mx-auto px-16 py-4">
@@ -20,14 +25,18 @@ const Navbar = () => {
         </ul>
 
         {/* hamburger menu */}
-        <div className="hamburger">
+        <div className="hamburger" ref={menu} onClick={()=>{
+          mobile.current.classList.toggle('activemobile')
+        }
+        }>
           <div className="ham"></div>
           <div className="ham"></div>
           <div className="ham"></div>
         </div>
 
         {/* Mobile Menu */}
-        <ul className="mobile-menu nav-list flex flex-row items-center gap-6 list-none cursor-pointer text-white">
+        <ul ref={mobile} 
+        className="mobile-menu nav-list flex flex-col items-center justify-center gap-6 list-none cursor-pointer text-white">
           <Link to="home" smooth={true} duration={500} activeClass='active' spy={true}><li>Home</li></Link>
           <Link to="about" smooth={true} duration={500} activeClass='active' spy={true}><li>About</li></Link>
           <Link to="projects" smooth={true} duration={500} activeClass='active' spy={true}><li>Projects</li></Link>

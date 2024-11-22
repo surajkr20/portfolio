f47914a62d74587fe5a10a763de5fc5ed9208eb1
surajkr20 <suraj71308kumar@gmail.com>
2024-11-22 06:14:07 +0530

    add hire me button

--- portfolio/src/components/Logo/Logo.jsx
+++ portfolio/src/components/Logo/Logo.jsx
@@ -1,20 +1,11 @@
-// import React from "react";
-
-import {Link} from 'react-scroll'
-import './Logo.css'
+import { Link } from "react-scroll";
 
 const Logo = () => {
   return (
-    <div id='logo' className="flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <Link
-        to="home"
-        className="logo w-12 h-12 text-white flex items-center text-2xl justify-center rounded-full font-bold cursor-pointer"
-
-        // framer-motion syntax
-        // whileHover={{
-        //     backgroundColor: ["#121212","rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","#121212"],
-        //     transition: {duration:1, repeat: Infinity}
-        // }}
+        to="home" smooth={true}
+        className="w-12 h-12 bg-[#340f0f] text-white flex items-center text-2xl justify-center rounded-full font-bold cursor-pointer"
       >
         <span className="text-pretty text-orange-600">s</span>k
       </Link>
--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -1,12 +1,27 @@
 // import React from 'react'
 import '../Home/Home.css'
+import man from "../../assets/man.png"
+// import TypingEffect from "react-typing-effect";
 
 const Home = () => {
   return (
-    <div id='home' className='w-full'>
-      Home
+    <div id='home' className=''>
+
+      <div className="left-home">
+        <div className="home-details">
+          <div className="line1">I&lsquo;m</div>
+          <div className="line2">Suraj Kumar</div>
+          <div className="line3">Programmer</div>
+        </div>
+        <button id='btn' className='w-[150px] p-2 bg-slate-500 rounded-md border-none mt-8 mr-[310px]'>Hire Me</button>
+      </div>
+
+      <div className="right-home">
+        <img src={man} alt="suraj kr" />
+      </div>
     </div>
   )
 }
 
 export default Home;
+

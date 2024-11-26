157d5939d0a4578e6b22d9b12357c0f19b4b7bb9
surajkr20 <suraj71308kumar@gmail.com>
2024-11-26 10:46:34 +0530

    skills page completed

--- portfolio/src/pages/Skills/Skills.jsx
+++ portfolio/src/pages/Skills/Skills.jsx
@@ -9,6 +9,10 @@ const Skill = ({ name, x, y }) => {
       className="flex items-center justify-center rounded-full font-semibold bg-black text-white shadow-black
         py-3 px-6 cursor-pointer absolute"
       whileHover={{ scale: 1.05 }}
+      initial={{x:0, y:0}}
+      whileInView={{x:x, y:y}}
+      transition={{duration: 1.5}}
+      // viewport={{once: true}}
     >
       {name}
     </motion.div>
@@ -17,19 +21,34 @@ const Skill = ({ name, x, y }) => {
 
 const Skills = () => {
   return (
-    <div id="skills">
-      <h2 className="font-bold text-8xl mt-10 text-center text-black">
+    <div id="skills" className="mt-8">
+      <h2 className="font-bold text-6xl text-center text-black">
         Skills
       </h2>
-      <div className="radical-circular relative flex items-center justify-center rounded-full">
+      <div className="radical-circular relative flex items-center justify-center rounded-full mt-6">
         <motion.div
-          className="flex items-center justify-center rounded-full font-semibold bg-black text-white p-5 shadow-black
-        cursor-pointer absolute"
+          className="flex items-center justify-center rounded-full text-2xl font-semibold bg-red-700 text-white py-2 px-6 shadow-black
+        cursor-pointer"
           whileHover={{ scale: 1.05 }}
         >
-          web
+          Skills
         </motion.div>
-        <Skill name="css" x='-5vw' y='-10vw'/>
+
+        <Skill name="C/C++" x='-30vw' y='-4vw'/>
+        {/* <Skill name="Data structure & Algorithm" x='-5vw' y='-10vw'/> */}
+
+        <Skill name="HTML" x='0' y='-20vw'/>
+        <Skill name="CSS" x='-20vw' y='-5vw'/>
+        <Skill name="Tailwind CSS" x='10vw' y='11vw'/>
+        <Skill name="Javascript" x='25vw' y='6vw'/>
+        <Skill name="Typescript" x='25vw' y='-2vw'/>
+        <Skill name="React.js" x='-6vw' y='12vw'/>
+        <Skill name="Next.js" x='-20vw' y='-15vw'/>
+        <Skill name="Node.js" x='10vw' y='-10vw'/>
+        <Skill name="Express.js" x='25vw' y='-14vw'/>
+        <Skill name="MongoDB" x='-5vw' y='-10vw'/>
+        <Skill name="Firebase" x='-20vw' y='6vw'/>
+
       </div>
     </div>
   );

12b40c84477375f48e019c9b40a6e77b905072d9
surajkr20 <suraj71308kumar@gmail.com>
2024-11-26 20:14:44 +0530

    add skills page responsiveness

--- portfolio/src/components/Logo/Logo.jsx
+++ portfolio/src/components/Logo/Logo.jsx
@@ -3,10 +3,7 @@ import {useGSAP} from '@gsap/react'
 import gsap from 'gsap'
 
 import './Logo.css'
-import {Link, Outlet} from "react-router-dom";
 import { motion } from "framer-motion";
-
-const MotionLink = motion(Link);
 
 const Logo = () => {
 
@@ -27,8 +24,7 @@ const Logo = () => {
 
   return (
     <div className="flex items-center justify-center" id="link">
-      <MotionLink
-        to="home" smooth={true}
+      <motion.div
         className="logo w-10 h-10 flex items-center text-2xl justify-center rounded-full font-bold cursor-pointer"
         whileHover={{
           backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
@@ -36,8 +32,7 @@ const Logo = () => {
         }}
       >
         <span id='logo-text' className='text-[20px]'>SK</span>
-      </MotionLink>
-      <Outlet/>
+      </motion.div>
     </div>
   );
 };
--- portfolio/src/pages/Skills/Skills.jsx
+++ portfolio/src/pages/Skills/Skills.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 const Skill = ({ name, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-black text-white shadow-black
+      className="skills-text flex items-center justify-center rounded-full font-semibold bg-black text-white shadow-black
         py-3 px-6 cursor-pointer absolute"
       whileHover={{ scale: 1.05 }}
       initial={{x:0, y:0}}
@@ -22,32 +22,30 @@ const Skill = ({ name, x, y }) => {
 const Skills = () => {
   return (
     <div id="skills" className="mt-8">
-      <h2 className="font-bold text-6xl text-center text-black">
+      <h2 className="skills-h2 font-bold text-4xl text-center text-black">
         Skills
       </h2>
       <div className="radical-circular relative flex items-center justify-center rounded-full mt-6">
         <motion.div
-          className="flex items-center justify-center rounded-full text-2xl font-semibold bg-red-700 text-white py-2 px-6 shadow-black
-        cursor-pointer"
+          className="center-skill flex justify-center rounded-full text-2xl font-semibold bg-red-700 text-white 
+          py-2 px-5 shadow-black cursor-pointer mt-[-55px] mr-4"
           whileHover={{ scale: 1.05 }}
         >
           Skills
         </motion.div>
 
-        <Skill name="C/C++" x='-30vw' y='-4vw'/>
-        {/* <Skill name="Data structure & Algorithm" x='-5vw' y='-10vw'/> */}
-
-        <Skill name="HTML" x='0' y='-20vw'/>
-        <Skill name="CSS" x='-20vw' y='-5vw'/>
-        <Skill name="Tailwind CSS" x='10vw' y='11vw'/>
-        <Skill name="Javascript" x='25vw' y='6vw'/>
-        <Skill name="Typescript" x='25vw' y='-2vw'/>
-        <Skill name="React.js" x='-6vw' y='12vw'/>
-        <Skill name="Next.js" x='-20vw' y='-15vw'/>
-        <Skill name="Node.js" x='10vw' y='-10vw'/>
-        <Skill name="Express.js" x='25vw' y='-14vw'/>
-        <Skill name="MongoDB" x='-5vw' y='-10vw'/>
-        <Skill name="Firebase" x='-20vw' y='6vw'/>
+        <Skill name="C/C++" x='-30vw' y='-10vw'/>
+        <Skill name="HTML" x='-5vw' y='-20vw'/>
+        <Skill name="CSS" x='-32vw' y='0vw'/>
+        <Skill name="Tailwind CSS" x='6vw' y='14vw'/>
+        <Skill name="Javascript" x='22vw' y='10vw'/>
+        <Skill name="Typescript" x='30vw' y='-6vw'/>
+        <Skill name="React.js" x='-12vw' y='14vw'/>
+        <Skill name="Next.js" x='-21vw' y='-18vw'/>
+        <Skill name="Node.js" x='10vw' y='-19vw'/>
+        <Skill name="Express.js" x='25vw' y='-16vw'/>
+        <Skill name="MongoDB" x='30vw' y='4vw'/>
+        <Skill name="Firebase" x='-27vw' y='10vw'/>
 
       </div>
     </div>
